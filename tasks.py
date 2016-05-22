569220534749a97993c495a1fa728a50e2c3821d
JR Minnaar <jr.minnaar@gmail.com>
2016-05-22 20:56:29 +0200

    Update Invoke tasks
    Invoke now includes most basic tasks for
    building, cleaning, installing and setting up
    development

--- tasks.py
+++ tasks.py
@@ -1,6 +1,17 @@
-from invoke import run, task
-from invoke.util import cd
+import os
+
+from invoke import run, task, env
+from invoke.util import cd, contextmanager
 import sys
+
+env.directory = './'
+env.activate = 'source env/bin/activate && '
+
+
+@task()
+def pip(cmd='list', venv=True):
+    venv = env.activate if venv else ''
+    run('{venv}pip {cmd}'.format(**locals()))
 
 
 @task
@@ -9,6 +20,12 @@ def mkdir(path):
         run('mkdir %s' % path)
     elif sys.platform == 'unix':
         run('mkdir -p %s' % path)
+
+
+@task
+def test_setup():
+    pip('install -r tests/requirements.txt')
+    pip('install -U .')
 
 
 @task
@@ -23,12 +40,24 @@ def create_test_app():
 
 
 @task
-def clean(docs=False, bytecode=False, extra=''):
-    patterns = ['build']
+def docs():
+    """Build html docs
+
+    :return:
+    """
+    with cd('docs'):
+        run('make html')
+
+
+@task
+def clean(docs=False, bytecode=False, venv=False, extra=''):
+    patterns = ['build', '*.egg-info', 'dist']
     if docs:
         patterns.append('docs/_build')
     if bytecode:
         patterns.append('**/*.pyc')
+    if venv:
+        patterns.append('env')
     if extra:
         patterns.append(extra)
     for pattern in patterns:
@@ -37,6 +66,11 @@ def clean(docs=False, bytecode=False, extra=''):
 
 @task
 def build(docs=False):
-    run("python setup.py build")
+    run("python setup.py sdist bdist_wheel")
     if docs:
         run("sphinx-build docs docs/_build")
+
+
+@task
+def runserver():
+    run("python test/manage.py runserver")
